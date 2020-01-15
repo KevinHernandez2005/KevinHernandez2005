4bee09e6a7e8007571eed20fdb363e7cfdc052ed
KevinHernandez2005 <55292353+KevinHernandez2005@users.noreply.github.com>
2020-01-15 13:20:13 -0500

    Update myScripts.js

--- tests/myScripts.js
+++ tests/myScripts.js
@@ -1,6 +1,37 @@
+var a;
+for (a=0; a<30; a++)
+  {
+    document.write("loop:" + a + "<br />");
+  }
+  
+var b;
+for (b=50; b>0; b--)
+  {
+    document.write("loop:" + (b + 150) + "<br />");
+  }
+
+var c;
+for (c=0; c<51; c++)
+  {
+    document.write("loop:" + (c*2) + "<br />");
+  }
+
+var d;
+for (d=0; d<5000; d+=100)
+  {
+    document.write("loop:" + d + "<br />");
+  }
+
+var e
+for (e=0; e<9; e++)
+  {
+    var e2 = Math.pow(2,e);
+    document.write("loop:" + ans + "<br />" )
+  }
+
 var i
 for (i=0; i<9; i++)
   {
-    var ans = Math.pow(2,i);
+    var i2 = Math.sqrt(i);
     document.write("loop:" + ans + "<br />" )
   }
